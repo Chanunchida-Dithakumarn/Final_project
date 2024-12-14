1d344a97578ad019bfdd2b80dda97c0506ae0dcc
Chanunchida Dithakumarn <chanunchida.d@ku.th>
2024-12-15 01:06:58 +0700

    draw, move and update_velocity method

---
+++ ball.py
@@ -0,0 +1,34 @@
+import turtle
+
+
+class Ball:
+    def __init__(self, color, size, x, y, vx, vy):
+        self.color = color
+        self.size = size
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.canvas_width = turtle.screensize()[0]
+        self.canvas_height = turtle.screensize()[1]
+
+    def draw(self):
+        turtle.penup()
+        turtle.color(self.color)
+        turtle.fillcolor(self.color)
+        turtle.goto(self.x, self.y - self.size)
+        turtle.pendown()
+        turtle.begin_fill()
+        turtle.circle(self.size)
+        turtle.end_fill()
+
+    def move(self, dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+
+    def update_velocity(self):
+        if abs(self.x) > (self.canvas_width - self.size):
+            self.vx = -self.vx
+
+        if abs(self.y) > (self.canvas_height - self.size):
+            self.vy = -self.vy
