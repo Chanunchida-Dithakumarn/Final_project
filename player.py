121170a069d7aa3480921b07d9f5ef770da9203e
Chanunchida Dithakumarn <chanunchida.d@ku.th>
2024-12-16 03:19:09 +0700

    add increase size method and add the player cannot move out of the canvas in all 4 moves method

--- player.py
+++ player.py
@@ -13,33 +13,40 @@ class Player:
         self.t.setheading(90)
         self.size = 1
 
+        self.canvas_width = turtle.screensize()[0]
+        self.canvas_height = turtle.screensize()[1]
+
     def move_up(self):
-        if self.t.heading() != 90:
-            self.t.setheading(90)
-            self.t.forward(15)
-        else:
-            self.t.forward(15)
+        if self.t.ycor() < self.canvas_height - self.size - 20:
+            if self.t.heading() != 90:
+                self.t.setheading(90)
+                self.t.forward(15)
+            else:
+                self.t.forward(15)
 
     def move_down(self):
-        if self.t.heading() != -90:
-            self.t.setheading(-90)
-            self.t.forward(15)
-        else:
-            self.t.forward(15)
+        if self.t.ycor() > -self.canvas_height + self.size + 20:
+            if self.t.heading() != -90:
+                self.t.setheading(-90)
+                self.t.forward(15)
+            else:
+                self.t.forward(15)
 
     def move_left(self):
-        if self.t.heading() != 180:
-            self.t.setheading(180)
-            self.t.forward(15)
-        else:
-            self.t.forward(15)
+        if self.t.xcor() > -self.canvas_width + self.size + 20:
+            if self.t.heading() != 180:
+                self.t.setheading(180)
+                self.t.forward(15)
+            else:
+                self.t.forward(15)
 
     def move_right(self):
-        if self.t.heading() != 0:
-            self.t.setheading(0)
-            self.t.forward(15)
-        else:
-            self.t.forward(15)
+        if self.t.xcor() < self.canvas_width - self.size - 20:
+            if self.t.heading() != 0:
+                self.t.setheading(0)
+                self.t.forward(15)
+            else:
+                self.t.forward(15)
 
     def increase_size(self):
         self.size = self.size + 0.5
