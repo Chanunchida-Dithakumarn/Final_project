f4562396a6f78505ba73f9b2644e0b5c8efc0424
Chanunchida Dithakumarn <chanunchida.d@ku.th>
2024-12-16 15:26:40 +0700

    clear code line that not use

--- score.py
+++ score.py
@@ -9,18 +9,12 @@ class Score:
         self.t.hideturtle()
         self.t.pencolor('mediumvioletred')
         self.t.penup()
-        self.t.goto(340, 240)
+        self.t.goto(330, 240)
         self.t.pendown()
-        # self.t.write(f"{self.score}", font=("Verdana", 32, "normal"))
         self.run()
 
     def run(self):
         self.t.clear()
-        # self.t.hideturtle()
-        # self.t.pencolor('mediumvioletred')
-        # self.t.penup()
-        # self.t.goto(340, 240)
-        # self.t.pendown()
         self.t.write(f"{self.score}", font=("Verdana", 32, "normal"))
 
     def update(self, new_score):
@@ -29,9 +23,3 @@ class Score:
 
     def __str__(self):
         return f"Score: {self.score}"
-
-
-
-# s = Score(15)
-# s.run()
-# turtle.done()
